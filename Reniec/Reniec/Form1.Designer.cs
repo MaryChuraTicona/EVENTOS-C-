febe5b9aea58d8f3ceaa5c79f66edb0bd8087710
franco23x <framangopu@upt.pe>
2025-11-19 06:33:43 -0500

    DNI

---
+++ Reniec/Reniec/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Reniec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.txtApePaterno = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtApeMaterno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(168, 97);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(128, 22);
+            this.txtDni.TabIndex = 0;
+            // 
+            // txtApePaterno
+            // 
+            this.txtApePaterno.Location = new System.Drawing.Point(168, 228);
+            this.txtApePaterno.Name = "txtApePaterno";
+            this.txtApePaterno.Size = new System.Drawing.Size(128, 22);
+            this.txtApePaterno.TabIndex = 1;
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(168, 170);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(128, 22);
+            this.txtNombres.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(453, 252);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtApeMaterno
+            // 
+            this.txtApeMaterno.Location = new System.Drawing.Point(168, 286);
+            this.txtApeMaterno.Name = "txtApeMaterno";
+            this.txtApeMaterno.Size = new System.Drawing.Size(128, 22);
+            this.txtApeMaterno.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "DNI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Apellido Materno:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Apellido Paterno:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Nombres";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtApeMaterno);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.txtApePaterno);
+            this.Controls.Add(this.txtDni);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.TextBox txtApePaterno;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtApeMaterno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Reniec/Reniec/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Newtonsoft.Json;
+
+namespace Reniec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public async Task<PersonaReniec> ConsultarDni(string dni)
+        {
+            string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImZyYW5jb21wMDI4QGdtYWlsLmNvbSJ9.JiCSOMkmlg_tbxXM1Hv_wgdsLsq2hhKM6NZsKb7iHU4"; // <-- Cambiar
+            string url = $"https://dniruc.apisperu.com/api/v1/dni/{dni}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                    return null;
+
+                PersonaReniec persona = JsonConvert.DeserializeObject<PersonaReniec>(json);
+                return persona;
+            }
+        }
+
+        private async void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string dni = txtDni.Text.Trim();
+
+            if (dni.Length != 8)
+            {
+                MessageBox.Show("Ingrese un DNI válido.");
+                return;
+            }
+
+            var persona = await ConsultarDni(dni);
+
+            if (persona == null)
+            {
+                MessageBox.Show("DNI no encontrado.");
+                return;
+            }
+
+            // Llenar tus textbox
+            txtNombres.Text = persona.nombres;
+            txtApePaterno.Text = persona.apellidoPaterno;
+            txtApeMaterno.Text = persona.apellidoMaterno;
+        }
+
+        
+    }
+}
